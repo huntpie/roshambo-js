5a35f8834ab8cce9bd98ccf99966d37452e377b9
Pierce Hunt <huntpie@gmail.com>
2021-02-15 23:08:50 -0500

    need to reset game, syntax messy

--- public/main.js
+++ public/main.js
@@ -18,6 +18,36 @@ function updatePlayer2ResultPaper(){
   player2ResultChoice.textContent = 'Paper'
 }
 
+function updatePlayer1ResultScissors() {
+  const player1ResultChoice = document.querySelector('h2.player1Choice')
+  player1ResultChoice.textContent = 'Scissors'
+}
+
+function updatePlayer2ResultScissors() {
+  const player2ResultChoice = document.querySelector('h2.player2Choice')
+  player2ResultChoice.textContent = 'Scissors'
+}
+
+function updatePlayer1ResultLizard() {
+  const player1ResultChoice = document.querySelector('h2.player1Choice')
+  player1ResultChoice.textContent = 'Lizard'
+}
+
+function updatePlayer2ResultLizard() {
+  const player2ResultChoice = document.querySelector('h2.player2Choice')
+  player2ResultChoice.textContent = 'Lizard'
+}
+
+function updatePlayer1ResultSpock() {
+  const player1ResultChoice = document.querySelector('h2.player1Choice')
+  player1ResultChoice.textContent = 'Spock'
+}
+
+function updatePlayer2ResultSpock() {
+  const player2ResultChoice = document.querySelector('h2.player2Choice')
+  player2ResultChoice.textContent = 'Spock'
+}
+
 function results() {
   const player1ResultChoice = document.querySelector('h2.player1Choice')
   const player2ResultChoice = document.querySelector('h2.player2Choice')
@@ -30,13 +60,25 @@ function results() {
     draw.textContent = 'Draw!'
     player1ResultChoice.style.visibility = 'visible'
     player2ResultChoice.style.visibility = 'visible'
-  } else if (( p1 === 'Paper' && p2 === 'Rock') || ( p1 === 'Scissors' && p2 === 'Paper')
+  } else if (( p1 === 'Paper' && p2 === 'Rock') || ( p1 === 'Scissors' && p2 === 'Paper') 
+  || ( p1 === 'Rock' && p2 === 'Scissors') || ( p1 === 'Scissors' && p2 === 'Lizard') 
+  || ( p1 === 'Lizard' && p2 === 'Spock') || ( p1 === 'Spock' && p2 === 'Scissors')
+  || ( p1 === 'Lizard' && p2 === 'Paper') || ( p1 === 'Spock' && p2 === 'Rock')
+  || ( p1 === 'Rock' && p2 === 'Lizard') || ( p1 === 'Paper' && p2 === 'Spock')
     ){
     const win1 = document.querySelector('h2.winnerName')
     win1.textContent = 'Player 1 Wins!'
-  } else if (( p1 === 'Rock' && p2 === 'Paper')) {
+    player1ResultChoice.style.visibility = 'visible'
+    player2ResultChoice.style.visibility = 'visible'
+  } else if (( p1 === 'Rock' && p2 === 'Paper') || ( p1 === 'Paper' && p2 === 'Scissors')
+      || ( p1 === 'Scissors' && p2 === 'Rock') || (p1 === 'Lizard' && p2 === 'Scissors')
+      || ( p1 === 'Spock' && p2 === 'Lizard') || ( p1 === 'Scissors' && p2 === 'Spock')
+      || ( p1 === 'Paper' && p2 === 'Lizard') || ( p1 === 'Rock' && p2 === 'Spock')
+      || ( p1 === 'Lizard' && p2 === 'Rock') || ( p1 === 'Spock' && p2 === 'Paper')) {
     const win2 = document.querySelector('h2.winnerName')
     win2.textContent = 'Player 2 Wins!'
+    player1ResultChoice.style.visibility = 'visible'
+    player2ResultChoice.style.visibility = 'visible'
   }
     }
 
@@ -49,15 +91,29 @@ function main() {
   document.querySelector('button.player2-rock').addEventListener('click', updatePlayer2ResultRock)
   document.querySelector('button.player1-rock').addEventListener('click', results)
   document.querySelector('button.player2-rock').addEventListener('click', results)
+  
 
   document.querySelector('button.player1-paper').addEventListener('click', updatePlayer1ResultPaper)
   document.querySelector('button.player2-paper').addEventListener('click', updatePlayer2ResultPaper)
   document.querySelector('button.player1-paper').addEventListener('click', results)
   document.querySelector('button.player2-paper').addEventListener('click', results)
   
-  }
+
+  document.querySelector('button.player1-scissors').addEventListener('click', updatePlayer1ResultScissors)
+  document.querySelector('button.player2-scissors').addEventListener('click', updatePlayer2ResultScissors)
+  document.querySelector('button.player1-scissors').addEventListener('click', results)
+  document.querySelector('button.player2-scissors').addEventListener('click', results)
   
-  
+  document.querySelector('button.player1-lizard').addEventListener('click', updatePlayer1ResultLizard)
+  document.querySelector('button.player2-lizard').addEventListener('click', updatePlayer2ResultLizard)
+  document.querySelector('button.player1-lizard').addEventListener('click', results)
+  document.querySelector('button.player2-lizard').addEventListener('click', results)
+
+  document.querySelector('button.player1-spock').addEventListener('click', updatePlayer1ResultSpock)
+  document.querySelector('button.player2-spock').addEventListener('click', updatePlayer2ResultSpock)
+  document.querySelector('button.player1-spock').addEventListener('click', results)
+  document.querySelector('button.player2-spock').addEventListener('click', results)
+}
 
   
 
